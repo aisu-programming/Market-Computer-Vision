340ecedfb96f53f0e73665ec9b332dc3432be9e8
aisu-programming <aisu.programming@gmail.com>
2021-10-29 00:51:22 +0800

    Update

---
+++ setup.sh
@@ -0,0 +1,23 @@
+wget https://www.piwheels.org/simple/h5py/h5py-2.10.0-cp37-cp37m-linux_armv7l.whl
+pip3 install h5py-2.10.0-cp37-cp37m-linux_armv7l.whl
+
+wget https://www.piwheels.org/simple/numpy/numpy-1.19.5-cp37-cp37m-linux_armv7l.whl
+pip3 install numpy-1.19.5-cp37-cp37m-linux_armv7l.whl
+
+wget https://www.piwheels.org/simple/grpcio/grpcio-1.32.0-cp37-cp37m-linux_armv7l.whl
+pip3 install grpcio-1.32.0-cp37-cp37m-linux_armv7l.whl
+
+wget https://github.com/bitsy-ai/tensorflow-arm-bin/releases/download/v2.4.0-rc4/tensorflow-2.4.0rc4-cp37-none-linux_armv7l.whl
+pip3 install tensorflow-2.4.0rc4-cp37-none-linux_armv7l.whl
+
+wget https://www.piwheels.org/simple/opencv-python/opencv_python-4.5.3.56-cp37-cp37m-linux_armv7l.whl
+pip3 install opencv_python-4.5.3.56-cp37-cp37m-linux_armv7l.whl
+
+wget https://www.piwheels.org/simple/scikit-image/scikit_image-0.18.3-cp37-cp37m-linux_armv7l.whl
+pip3 install scikit_image-0.18.3-cp37-cp37m-linux_armv7l.whl
+
+wget https://www.piwheels.org/simple/matplotlib/matplotlib-3.4.3-cp37-cp37m-linux_armv7l.whl
+pip3 install matplotlib-3.4.3-cp37-cp37m-linux_armv7l.whl
+
+pip3 install pycocotools
+pip3 install tqdm
